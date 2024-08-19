2f2935ff291697c1dc958316a03b95718bcc3fc0
oleksandr.piskurov <oleksandr.piskurov@mac-MT41YG9TJ6.local>
2024-08-19 13:56:42 +0300

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,15 @@
 FROM ubuntu:latest
 LABEL authors="oleksandr.piskurov"
 
-ENTRYPOINT ["top", "-b"]
+RUN apt update
+RUN apt install -y git npm
+
+WORKDIR /usr/app
+COPY ./ /usr/app
+
+RUN npm init -y
+RUN npm install express body-parser
+RUN npm install --save-dev typescript @types/express nodemon ts-node
+RUN npm install -g ts-node typescript '@types/node'
+
+ENTRYPOINT ["ts-node", "typescript-file.ts"]
--- run.sh
+++ run.sh
@@ -1,2 +1,25 @@
 echo 'here is script'
 docker pull lolvalolee/calendar:latest
+
+pipeline {
+  agent any
+  stages {
+    stage('HelloWorld') {
+      steps {
+        echo 'Hello World'
+      }
+    }
+    stage('git clone') {
+      steps {
+         cleanWs()
+         sh 'git clone https://github.com/lolvalolee/calendar_jenkins.git'
+         sh 'ls -la'
+         sh 'chmod +x ./calendar_jenkins/run.sh'
+         script {
+             img = 'lolvalolee/calendar:latest'
+             docker.image("${img}").run('-d -p 80:80')
+         }
+      }
+    }
+  }
+}
